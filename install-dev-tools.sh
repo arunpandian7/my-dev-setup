b6e9e2b90621b082633d3d9532bb726b49515fe1
Arun Pandian R <arunrk7.leo@gmail.com>
2021-01-15 11:23:02 +0530

    Added Dev Tools Installer

---
+++ install-dev-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+#Installing Visual Studio Code
+wget -O ~/Downloads/code.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+sudo apt install ~/Downloads/code.deb
+
+#Install Python and Pip
+sudo apt install python3 python3-pip
